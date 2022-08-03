a9b44f3b7b67899e00383ec4224860a01b6339f1
aristianes <71615848+aristianes@users.noreply.github.com>
2022-08-03 10:40:37 -0500

    Created product object
    Today I created the product object, in model package.

---
+++ src/main/java/com/aristiane/store/model/Produto.java
@@ -0,0 +1,44 @@
+package com.aristiane.store.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String marca;
+	private int quantidade;
+	private int preco;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public int getPreco() {
+		return preco;
+	}
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+	
+
+}
