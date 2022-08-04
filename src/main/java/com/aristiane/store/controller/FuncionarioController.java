aa7ed07536be1ad66f2b39d2e8e6d809eda48da9
aristianes <71615848+aristianes@users.noreply.github.com>
2022-08-04 10:34:17 -0500

    feature get funcionario
    Created and validate.

--- src/main/java/com/aristiane/store/controller/FuncionarioController.java
+++ src/main/java/com/aristiane/store/controller/FuncionarioController.java
@@ -1,6 +1,9 @@
 package com.aristiane.store.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,12 @@ public class FuncionarioController {
 	public Funcionario save(@RequestBody Funcionario funcionario){
 		return this.service.save(funcionario);
 		
+	}
+	
+	@GetMapping 
+	public List<Funcionario>findAll() {
+        return service.findAll();
+        
 	}
 	
 }
--- src/main/java/com/aristiane/store/service/FuncionarioService.java
+++ src/main/java/com/aristiane/store/service/FuncionarioService.java
@@ -1,12 +1,12 @@
 package com.aristiane.store.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aristiane.store.model.Funcionario;
 import com.aristiane.store.repository.FuncionarioRepository;
-
-
 
 @Service
 public class FuncionarioService {
@@ -16,8 +16,15 @@ public class FuncionarioService {
 
 	public Funcionario save(Funcionario funcionario) {
 		this.repository.save(funcionario);
-		
+
 		return funcionario;
+	}
+
+	public List<Funcionario> findAll() {
+		List<Funcionario> funcionarios = this.repository.findAll();
+
+		return funcionarios;
+
 	}
 
 }
